a7a269dbb3e44b3d307dc30ab488d3fb3f862018
LVIANACODE <lucas_viana99@outlook.com>
2022-08-31 21:16:52 -0300

    Add naive solution

---
+++ Sandbox/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yesCounter = 1;
+            int noCounter = 2;
+            int maybeCounter = 3;
+
+            int total = yesCounter + noCounter + maybeCounter;
+
+            var yesPercent = Math.Round(yesCounter * 100.0 / total, 2);
+            var noPercent = Math.Round(noCounter * 100.0 / total, 2);
+            var maybePercent = Math.Round(maybeCounter * 100.0 / total, 2);
+
+            var excess = Math.Round(100 - yesCounter - noPercent - maybePercent, 2);
+            Console.WriteLine($"Excess: {excess}");
+          
+            if(yesCounter > noCounter)
+            {
+                if(yesCounter > maybeCounter)
+                {
+                    Console.WriteLine($"Yes Won");
+                    yesPercent += excess;
+                }
+                else if (maybeCounter > yesCounter)
+                {
+                    Console.WriteLine($"Won");
+                    maybePercent += excess;
+                }
+                else
+                {
+                    Console.WriteLine($"DRAW");
+                    noPercent += excess;
+                }
+            }
+            else if (noCounter > yesCounter)
+            {
+                if (noCounter > maybeCounter)
+                {
+                    Console.WriteLine($"No Won");
+                    noPercent += excess;
+                }
+                else if (maybeCounter > noCounter)
+                {
+                    Console.WriteLine($"Won");
+                    maybePercent += excess;
+                }
+                else
+                {
+                    Console.WriteLine($"DRAW");
+                    yesPercent += excess;
+                }
+            }
+            else if(maybeCounter > yesCounter)
+            {
+                Console.WriteLine($"Maybe Won");
+                maybePercent += excess;
+            }
+            else
+            {
+                Console.WriteLine($"DRAW");
+            }
+
+            Console.WriteLine($"Yes Counts: {yesCounter}, Percentage: {Math.Round(yesPercent, 2)}%");
+            Console.WriteLine($"No Counts: {noCounter}, Percentage: {Math.Round(noPercent, 2)}%");
+            Console.WriteLine($"Maybe Counts: {maybeCounter}, Percentage: {Math.Round(maybePercent, 2)}%");
+
+            Console.WriteLine($"Total Percentage: {Math.Round(yesCounter + noPercent + maybePercent)}%");
+        }
+    }
+}
